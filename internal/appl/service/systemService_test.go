6a5b06519fa15cd36eeaf5ca08171016b4826607
Jonatas do Nascimento Freitas <jonatasfreitas008@gmail.com>
2025-08-24 13:53:08 -0300

    Update systemService_test.go

--- internal/appl/service/systemService_test.go
+++ internal/appl/service/systemService_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestSendEmail(t *testing.T) {
 	t.Run("teste positivo", func(t *testing.T) {
 		systemService := NewSystemService()
-		err := systemService.SendEmail("<h1>isso é um teste</h1>", "kiramy763@gmail.com", "isso é um teste")
+		err := systemService.SendEmail("<h1>isso é um teste</h1>", "testeservice123@gmail.com", "isso é um teste")
 		if err != nil {
 			t.Error(err)
 		}
